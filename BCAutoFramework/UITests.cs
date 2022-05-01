37b1c0f7b80f132dd25f4a656157253fdc42608c
Bill crowe <billcrowe22@gmail.com>
2022-05-01 10:09:18 +0100

    new project files for webtest automation using selenium 4.0

---
+++ BCAutoFramework/UITests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace BCAutoFramework
+{
+    public class UITests
+    {
+        public IWebDriver Driver;
+        public WebDriverWait wait;
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void navigateToWebsite()
+        {
+            Driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+            Console.WriteLine("Test1");
+            Assert.Pass();
+        }
+    }
+}
